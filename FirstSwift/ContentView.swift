72d08e5b32dae67180e2053fe4235f3e94a8aab0
ev3cary <ev3cary@gmail.com>
2025-01-25 10:49:12 -0500

    2nd push

--- FirstSwift/ContentView.swift
+++ FirstSwift/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world! 1st Try")
+            Text("Hello, world! 2nd Try")
         }
         .padding()
     }

59f44baddb0d2e48a1815944c902ff89237137f0
ev3cary <ev3cary@gmail.com>
2025-01-25 10:41:46 -0500

    Initial Commit

---
+++ FirstSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirstSwift
+//
+//  Created by Yi Yang on 1/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirstSwift/FirstSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftApp.swift
+//  FirstSwift
+//
+//  Created by Yi Yang on 1/25/25.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
